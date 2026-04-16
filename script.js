b8827801a451a447935109ea5d225d017d745c21
João Victor <102191447+jaoz1nn@users.noreply.github.com>
2026-04-16 10:36:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-  let noBtn = document.getElementById("no");
+  let yes = document.getElementById("yes");
+  let no = document.getElementById("no");
 
   let q1 = document.getElementById("q1");
   let q2 = document.getElementById("q2");
@@ -8,27 +9,25 @@ document.addEventListener("DOMContentLoaded", function () {
 
   let audio = document.getElementById("bg-music");
 
-  function trocarTela(atual, proxima) {
-    atual.style.display = "none";
-    proxima.style.display = "block";
-  }
-
   // botão SIM
-  document.getElementById("yes").addEventListener("click", function () {
-    trocarTela(q1, q2);
+  yes.addEventListener("click", function () {
+    q1.style.display = "none";
+    q2.style.display = "block";
   });
 
   // botão NÃO foge
-  noBtn.addEventListener("mouseover", function () {
-    let x = Math.random() * 250 - 125;
-    let y = Math.random() * 250 - 125;
-    noBtn.style.transform = `translate(${x}px, ${y}px)`;
+  no.addEventListener("mouseover", function () {
+    let x = Math.random() * 200 - 100;
+    let y = Math.random() * 200 - 100;
+    no.style.transform = `translate(${x}px, ${y}px)`;
   });
 
   // escolha da data
   document.querySelectorAll(".date").forEach(function (btn) {
     btn.addEventListener("click", function () {
-      trocarTela(q2, q3);
+      q2.style.display = "none";
+      q3.style.display = "block";
+
       audio.play();
     });
   });
